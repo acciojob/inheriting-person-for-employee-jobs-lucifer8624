ccede198c27c265dbb4f366f8c844cf9ed6f31e9
Raza Sultan <120782387+lucifer8624@users.noreply.github.com>
2025-10-28 15:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// complete this js code
-function Person(name, age) {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function Employee(name, age, jobTitle) {}
+  // Method to greet
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
 
-// Do not change code below this line
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call the parent constructor
+    this.jobTitle = jobTitle;
+  }
+
+  // Method to greet with job title
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  }
+}
+
+// Expose classes globally for Cypress tests
 window.Person = Person;
 window.Employee = Employee;
+
+
