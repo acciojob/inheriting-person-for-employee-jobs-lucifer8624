266b1964a5b4885ba9aa0bf796bb81eb9e7d42ed
Raza Sultan <120782387+lucifer8624@users.noreply.github.com>
2025-10-28 16:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
   }
 
   // Method to greet
   greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
   }
 }
 
 class Employee extends Person {
   constructor(name, age, jobTitle) {
-    super(name, age); // Call the parent constructor
-    this.jobTitle = jobTitle;
+    super(name, age); // call parent constructor
+    this._jobTitle = jobTitle;
+  }
+
+  // Getter for jobTitle
+  get jobTitle() {
+    return this._jobTitle;
   }
 
   // Method to greet with job title
@@ -24,8 +39,6 @@ class Employee extends Person {
   }
 }
 
-// Expose classes globally for Cypress tests
+// Expose globally for Cypress
 window.Person = Person;
 window.Employee = Employee;
-
-
